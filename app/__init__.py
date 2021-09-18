e67a0b913904f92d1ad2dc90ce75fd210d548f8e
Kipkorir2017 <pkobenj41@gmail.com>
2021-09-19 01:40:59 +0300

    add email files

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
-# import os
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -12,6 +12,7 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+mail = Mail()
 
 photos = UploadSet('photos',IMAGES)
 
@@ -31,7 +32,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-
+    mail.init_app(app)
      # configure UploadSet
     configure_uploads(app,photos)
     # Registering the blueprint
--- config.py
+++ config.py
@@ -6,6 +6,13 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Benja312@localhost/pitches'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("tonuikipkorir1@gmail.com")
+    MAIL_PASSWORD = os.environ.get("password")
 class ProdConfig(Config):
     pass
 
