9b6fc81a542d0c129a680256a9a9f3203111b86c
Kipkorir2017 <pkobenj41@gmail.com>
2021-09-18 20:37:21 +0300

    add column for pass secure in the user model class

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
 
 
@@ -10,7 +11,20 @@ class User(db.Model):
     
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
     def __repr__(self):
         return f'User {self.username}'
     
@@ -34,11 +48,11 @@ class Pitch(db.Model):
 
 
 
-    # all_pitches=[]
-    # def __init__(self,pitch_id,pitch_title,postedOn):
-    #     self.pitch_id=pitch_id
-    #     self.pitch_title=pitch_title
-    #     self.postedOn=postedOn
+    all_pitches=[]
+    def __init__(self,pitch_id,pitch_title,postedOn):
+        self.pitch_id=pitch_id
+        self.pitch_title=pitch_title
+        self.postedOn=postedOn
         
         
     def save_pitch(self):
