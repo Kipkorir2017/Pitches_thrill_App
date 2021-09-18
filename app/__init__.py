000b95537eacdecca34055870e0987f5c035e584
Kipkorir2017 <pkobenj41@gmail.com>
2021-09-19 01:23:59 +0300

    install flask_mail

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
-
+# import os
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -13,9 +13,8 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
-
-
 photos = UploadSet('photos',IMAGES)
+
 def create_app(config_name):
 
     app = Flask(__name__)
@@ -23,15 +22,18 @@ def create_app(config_name):
     #default value during deployment
     app.secret_key='I LOVE PYTHON'
 
-    # configure UploadSet
-    configure_uploads(app,photos)
+   
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    # app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd()
 
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+
+     # configure UploadSet
+    configure_uploads(app,photos)
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,11 @@
-from flask import render_template,redirect, url_for,abort
+from flask import render_template,redirect,request, url_for,abort
 # from app import app
 from . import main
 from wtforms import form
 from .forms import PitchesForm,UpdateProfile
 from ..models import Pitch,User
 from flask_login import login_required
-from .. import db
+from .. import db,photos
 
 #views
 @main.route('/')
@@ -46,6 +46,18 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
 
 
 
